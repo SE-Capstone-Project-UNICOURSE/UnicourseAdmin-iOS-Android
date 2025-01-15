ca6911268d6dc9eb13c3127ab078ca34d4673810
Kane <trungkiennguyen0310@gmail.com>
2025-01-15 09:37:19 +0700

    fixing android folder project

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
+import 'react-native-gesture-handler';
 
 const theme = createTheme({
   lightColors: {
