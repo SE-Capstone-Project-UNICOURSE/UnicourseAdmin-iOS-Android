4b79eb3e348a72c5301ab898c096ec79c13810d5
Kane <trungkiennguyen0310@gmail.com>
2025-01-16 16:30:47 +0700

    update payment view

--- src/features/payments/views/screens/PaymentScreen.ios.tsx
+++ src/features/payments/views/screens/PaymentScreen.ios.tsx
@@ -1,11 +1,51 @@
+import { Card, ListItem } from '@rneui/base';
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const PaymentScreen = () => {
+  const payments = [
+    {
+      title: 'Children Care',
+      products: '45',
+      revenue: '$44,320',
+      customers: '12,422',
+    },
+    {
+      title: 'More Vapor',
+      products: '199',
+      revenue: '$123,320',
+      customers: '89,212',
+    },
+  ];
+
   return (
     <SafeAreaView>
-      <Text>PaymentScreen iOS</Text>
+      <View>
+        {payments.map((payment, index) => (
+          <Card key={index} containerStyle={{ borderRadius: 20, marginBottom: 20 }}>
+            <Card.Title>{payment.title}</Card.Title>
+            <ListItem bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>Products</ListItem.Title>
+                <ListItem.Subtitle>{payment.products}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem>
+              <ListItem.Content>
+                <ListItem.Title>Revenue</ListItem.Title>
+                <ListItem.Subtitle>{payment.revenue}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem>
+              <ListItem.Content>
+                <ListItem.Title>Customers</ListItem.Title>
+                <ListItem.Subtitle>{payment.customers}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          </Card>
+        ))}
+      </View>
     </SafeAreaView>
   );
 };
