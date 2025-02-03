866f827bcc3300dcddaea4c7bb015b3b054da111
Kane <trungkiennguyen0310@gmail.com>
2025-02-03 11:02:50 +0700

    add simple native local storage with turbo modules in iOS

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  UnicourseAdmin
+//
+//  Created by Kane Nguyen on 3/2/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
